c638f001088589c94d1855b4090c781b5a8e95bb
Brooke Heathcock <atomicgardeninteractive@gmail.com>
2025-02-05 15:43:12 +0000

    GPS Distance Calc

--- Newquay Map Game/Assets/Scripts/GameHandler.cs
+++ Newquay Map Game/Assets/Scripts/GameHandler.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class GameHandler : MonoBehaviour
 
@@ -12,9 +13,16 @@ public class GameHandler : MonoBehaviour
     public float decayRate = 0.01f;
     public float distanceThreshold = 0.01f;
 
+    public Pos[] pins;
+    public int closestPin;
+    public double closestDist;
+
+    Pos currentPos;
+
     Slider economySlider;
     Slider environmentSlider;
     Slider housingSlider;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,6 +44,22 @@ public class GameHandler : MonoBehaviour
         economySlider.value = economyHealth;
         environmentSlider.value = environmentHealth;
         housingSlider.value = housingDemand;
+
+        // Update position
+        this.currentPos.lat = Input.location.lastData.latitude;
+        this.currentPos.lon = Input.location.lastData.longitude;
+
+        // Find closest pin
+        this.closestDist = Double.PositiveInfinity;
+        for (int i = 0; i < pins.Length; i++)
+        {
+            double d = this.currentPos.Distance(this.pins[i]);
+            if (d < this.closestDist)
+            {
+                this.closestDist = d;
+                this.closestPin = i;
+            }
+        }
     }
 
     public void economy_increase() 
@@ -46,5 +70,29 @@ public class GameHandler : MonoBehaviour
     public void enviroment_decrease() 
     {
         environmentHealth = environmentHealth - 0.1f;
+    }
+}
+
+[Serializable]
+public class Pos
+{
+    public string name;
+    public double lon;
+    public double lat;
+
+    public Pos(double lon, double lat)
+    {
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public double Distance(Pos to)
+    {
+        double r = 6371;
+        double p = Math.PI / 180;
+        double a = 0.5 - Math.Cos((to.lat - this.lat) * p) / 2
+                      + Math.Cos(this.lat * p) * Math.Cos(to.lat * p) *
+                        (1 - Math.Cos((to.lon - this.lon) * p)) / 2;
+        return 2 * r * Math.Asin(Math.Sqrt(a));
     }
 }
